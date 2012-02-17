191b4e094db39bd765987ac33e766dc25140abf7
OndrejSlamecka <ondrej@slamecka.cz>
2012-02-17 20:50:51 +0100

    Typo, whitespaces

--- app/BackendCommons/presenters/BasePresenter.php
+++ app/BackendCommons/presenters/BasePresenter.php
@@ -3,9 +3,9 @@
  * Part of CoolMS Content Management System
  *
  * @copyright (c) 2011 Ondrej Slamecka (http://www.slamecka.cz)
- * 
+ *
  * License within file license.txt in the root folder.
- * 
+ *
  */
 
 namespace Backend;
