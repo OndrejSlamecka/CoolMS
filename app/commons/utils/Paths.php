2c2185c95ceb84e70d718082a3623cfe72a76251
OndrejSlamecka <ondrej@slamecka.cz>
2012-02-20 13:22:42 +0100

    Application\Utils\Paths: Fixed escaping

--- app/commons/utils/Paths.php
+++ app/commons/utils/Paths.php
@@ -3,9 +3,9 @@
  * Part of CoolMS Content Management System
  *
  * @copyright (c) 2011 Ondrej Slamecka (http://www.slamecka.cz)
- * 
+ *
  * License within file license.txt in the root folder.
- * 
+ *
  */
 
 namespace Application\Utils;
@@ -18,14 +18,14 @@ class Paths extends \Nette\Object
     /**
      * Converts backslashes to forward slashes, removes redundant slashes and double dots ("..")
      * @param string $path
-     * @return string 
+     * @return string
      */
     public static function sanitize($path)
     {
-        // Use just '/' everywhere        
+        // Use just '/' everywhere
         $path = str_replace('\\', '/', $path); // Strings::replace($path, '~\\\~', '/');
-        // Use just one separator... 
-        $path = str_replace('\/\/', '/', $path); // Strings::replace($path, '~\/\/~', '/');
+        // Use just one separator...
+        $path = str_replace('//', '/', $path); // Strings::replace($path, '~\/\/~', '/');
         // Remove ..
         $path = str_replace('..', '', $path); // Strings::replace($path, '~\.\.~', '');
 
