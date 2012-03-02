dca55b13d7a4b92a1d9b6bcc943eba9668ca9b57
OndrejSlamecka <ondrej@slamecka.cz>
2012-03-02 17:39:37 +0100

    Application\RepositoryManager: method_exists instead of reflection (thanks HosipLan)

--- app/commons/models/RepositoryManager.php
+++ app/commons/models/RepositoryManager.php
@@ -13,19 +13,19 @@ namespace Application;
 class RepositoryManager extends \NDBF\RepositoryManager
 {
 
-    /** @var \Nette\DI\Container */
-    private $container;
+	/** @var \Nette\DI\Container */
+	private $container;
 
-    public function setContainer(\Nette\DI\Container $container)
-    {
-        $this->container = $container;
-    }
+	public function setContainer(\Nette\DI\Container $container)
+	{
+		$this->container = $container;
+	}
 
-    protected function onRepositoryCreated(\NDBF\Repository $instance)
-    {
-        parent::onRepositoryCreated($instance);
-        if ($instance->getReflection()->hasMethod('setContainer'))
-            $instance->setContainer($this->container);
-    }
+	protected function onRepositoryCreated(\NDBF\Repository $instance)
+	{
+		parent::onRepositoryCreated($instance);
+		if (method_exists($instance, 'setContainer'))
+			$instance->setContainer($this->container);
+	}
 
 }
