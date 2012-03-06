ef36a784bfff91c33abb397cd581a0cc5728aa16
OndrejSlamecka <ondrej@slamecka.cz>
2012-03-06 22:17:42 +0100

    AuthenticationModule: New password form has its own class

--- app/BackendCore/AuthenticationModule/presenters/BackendPresenter.php
+++ app/BackendCore/AuthenticationModule/presenters/BackendPresenter.php
@@ -18,119 +18,89 @@ namespace AuthenticationModule;
 class BackendPresenter extends \Backend\BasePresenter
 {
 
-    public function startup()
-    {
-        parent::startup();
-        $this->setLayout('layout'); // This - default layout - was overriden in BasePresenter
-    }
+	public function startup()
+	{
+		parent::startup();
+		$this->setLayout('layout'); // This - default layout - was overriden in BasePresenter
+	}
 
-    /* ------------------------------- LOGIN -------------------------------- */
+	/* ------------------------------- LOGIN -------------------------------- */
 
-    /**
-     * Login form component factory.
-     */
-    protected function createComponentLoginForm()
-    {
+	/**
+	 * Login form component factory.
+	 */
+	protected function createComponentLoginForm()
+	{
 		return new LoginForm();
-    }
+	}
 
-    public function renderLogin()
-    {
-        //dump( Authenticator::hashPassword( 'email', 'password' ) ); // Printing for development purposes
-    }
+	public function renderLogin()
+	{
+		//dump( Authenticator::hashPassword( 'email', 'password' ) ); // Printing for development purposes
+	}
 
-    /* ------------------------------ LOGOUT -------------------------------- */
+	/* ------------------------------ LOGOUT -------------------------------- */
 
-    public function actionLogout()
-    {
-        $this->getUser()->logout(TRUE);
-        $this->redirect(':Authentication:Backend:login');
-    }
+	public function actionLogout()
+	{
+		$this->getUser()->logout(TRUE);
+		$this->redirect(':Authentication:Backend:login');
+	}
 
-    /* ----------------------- REQUEST NEW PASSWORD ------------------------- */
+	/* ----------------------- REQUEST NEW PASSWORD ------------------------- */
 
-    public function createComponentRequestNewPasswordForm()
-    {
+	public function createComponentRequestNewPasswordForm()
+	{
 		return new RequestNewPasswordForm($this->repositories->User);
-    }
+	}
 
-    public function actionCreatePassword($token, $newuser = false)
-    {
-        // Following action: newPasswordSuccess!
+	public function actionCreatePassword($token, $newuser = false)
+	{
+		// Following action: newPasswordSuccess!
 
-        /*
-         * Whether token is right is tested by fetching user by token
-         * It's validity is tested by Authenticator::isTokenValid()
-         *   with token creation time provided from user's row in database
-         */
+		/*
+		 * Whether token is right is tested by fetching user by token
+		 * It's validity is tested by Authenticator::isTokenValid()
+		 *   with token creation time provided from user's row in database
+		 */
 
-        // If user hasn't submitted new password (i.e. he comes from email)
-        if (!$this['newPassword']->isSubmitted()) {
+		// If user hasn't submitted new password (i.e. he comes from email)
+		if (!$this['newPassword']->isSubmitted()) {
 
-            $users = $this->repositories->User;
-            $user = $users->find(array('token' => $token))->fetch();
+			$users = $this->repositories->User;
+			$user = $users->find(array('token' => $token))->fetch();
 
-            if (!$user) {
-                $this->flashMessage('Verification failure. Please check if you have copied the link right.');
-                $this->redirect('login');
-            }
+			if (!$user) {
+				$this->flashMessage('Verification failure. Please check if you have copied the link right.');
+				$this->redirect('login');
+			}
 
-            $user = $user->toArray();
+			$user = $user->toArray();
 
-            if (!Authenticator::isTokenValid($user['token_created'])) {
-                $this->flashMessage('Verification failure. You have waited too long before reseting password. Please use "I forgot my password" again.');
-                $this->redirect('login');
-            }
+			if (!Authenticator::isTokenValid($user['token_created'])) {
+				$this->flashMessage('Verification failure. You have waited too long before reseting password. Please use "I forgot my password" again.');
+				$this->redirect('login');
+			}
 
-            // Create new token
-            $token = Authenticator::createToken();
-            $user['token'] = $token;
-            $user['token_created'] = new \DateTime();
-            $users->save($user, 'email');
+			// Create new token
+			$token = Authenticator::createToken();
+			$user['token'] = $token;
+			$user['token_created'] = new \DateTime();
+			$users->save($user, 'email');
 
-            $token_sess = $this->getSession('token');
-            $token_sess->value = $token;
-        } else {
-            if ($newuser) {
-                $userinfo = $this->getSession('userinfo');
-                $userinfo->newUser = true;
-            }
-        }
-    }
+			$token_sess = $this->getSession('token');
+			$token_sess->value = $token;
+		} else {
+			if ($newuser) {
+				$userinfo = $this->getSession('userinfo');
+				$userinfo->newUser = true;
+			}
+		}
+	}
 
-    public function createComponentNewPassword($name)
-    {
-        $form = new \Application\Form($this, $name);
-        $form->addPassword('password', 'New password');
-        $form->addSubmit('save', 'Save');
-        $form->onSuccess[] = array($this, 'newPasswordSuccess');
-        return $form;
-    }
-
-    public function newPasswordSuccess($form)
-    {
-        // Following action: login
-
-        $form = $form->getValues();
-        $token_sess = $this->getSession('token');
-        $token = $token_sess->value;
-
-        $users = $this->repositories->User;
-        $user = $users->find(array('token' => $token))->fetch();
-
-        if (!$user || !Authenticator::isTokenValid($user['token_created'])) {
-            $this->flashMessage('Verification failure. Please use "I forgot my password" again.');
-            $this->redirect('login');
-        }
-
-        $user = $user->toArray();
-        $user['password'] = Authenticator::calculateHash($form['password'], $user['salt']);
-        $user['token'] = null;
-        $user['token_created'] = null;
-        $users->save($user, 'email');
-
-        $this->flashMessage('Password was set. Now you can log in!');
-        $this->redirect('login');
-    }
+	public function createComponentNewPassword($name)
+	{
+		return new NewPasswordForm($this->repositories->User);
+	}
 
 }
