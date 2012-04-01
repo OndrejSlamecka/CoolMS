a1d837fc2f69aee95f5eaa082bd240d3dbe8c556
OndrejSlamecka <ondrej@slamecka.cz>
2012-04-02 01:56:04 +0200

    Typo; Frontend\Router does not to have such a fancy header

--- app/FrontendCommons/Router.php
+++ app/FrontendCommons/Router.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * Part of CoolMS Content Management System
- *
- * @copyright (c) 2011 Ondrej Slamecka (http://www.slamecka.cz)
- *
- * License within file license.txt in the root folder.
- *
- */
 
 namespace Frontend;
 
